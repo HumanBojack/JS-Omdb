58852410a59f257dc07eb96dc70409ba5df1dedf
HumanBojack <spychalar59@gmail.com>
2021-04-22 14:36:25 +0200

    Popup window with movie info (only works with api key)

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 let searchButton = document.querySelector("input[type='submit']");
 const APIKEY = "";
+let popup = document.getElementById("movie-popup");
+let closeBtn = document.getElementById("close_btn");
 
-const fetchOMDb = (title) => { // First one will use the api and second one will use the file (with harry potter as title)
-	// return fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${title}&type=movie`) //t= for movie, s= for search, also be carefull with spaces
+const fetchOMDbSearch = (title) => { // First one will use the api and second one will use the file (with harry potter as title)
+	return fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&s=${title}&type=movie`) //t= for movie, s= for search
+		.then(data => data.json())
+		.catch(error => console.error(error));
+
+	// return fetch(`search.json`)
 	// 	.then(data => data.json())
 	// 	.catch(error => console.error(error));
+}
 
-	return fetch(`search.json`)
+const fetchOMDbMovie = (id) => {
+	return fetch(`http://www.omdbapi.com/?apikey=${APIKEY}&i=${id}`)
 		.then(data => data.json())
 		.catch(error => console.error(error));
 }
 
-function onLearnMoreClick(event){
+async function onLearnMoreClick(event) {
 	event.preventDefault();
-	console.log(this.id);
+	let popup = document.getElementById("movie-popup");
+	popup.style.display = "block";
+	let movie = await fetchOMDbMovie(this.id);
+	let poster = document.getElementById("popup-poster");
+	let title = document.getElementById("popup-title");
+	let plot = document.getElementById("popup-plot");
+	let date = document.getElementById("popup-date");
+	let genres = document.getElementById("popup-genres");
+	let ratings = document.getElementById("popup-ratings");
+
+	poster.src = movie.Poster
+	title.innerHTML = `${movie.Title} <span id="close_btn">&times;</span>`;
+	plot.innerHTML = movie.Plot;
+	date.innerHTML = movie.Released;
+	genres.innerHTML = movie.Genre;
+	if (movie.Ratings && movie.Ratings.length >= 1){
+		ratings.innerHTML = `${movie.Ratings[0].Source} : ${movie.Ratings[0].Value}`
+	}
+
+	let closeBtn = document.getElementById("close_btn");
+	closeBtn.addEventListener("click", function(){
+		popup.style.display = "none";
+	})
 }
 
 async function updateList() {
 	let list = document.getElementById("movies_list");
 	let searchInput = document.getElementById("movie_search");
-	let result = await fetchOMDb(searchInput.value);
+	let result = await fetchOMDbSearch(encodeURIComponent(searchInput.value));
 	
 	list.innerHTML = "";
 	result.Search.forEach(movie => {
@@ -40,3 +70,12 @@ async function updateList() {
 }
 
 searchButton.addEventListener("click", updateList);
+closeBtn.addEventListener("click", function(){
+	popup.style.display = "none";
+})
+
+window.addEventListener("click", function(event){
+	if (event.target == popup){
+		popup.style.display = "none";
+	}
+})
